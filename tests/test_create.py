0f6b13a029e7741c0a1ba8edc09bc99083f4f602
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-12-12 13:09:45 +0000

    feat: add logging to pytest

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def test_create_resources(resources, app):
     """
     This test runs through all resources (in order) and makes sure they can be posted to the app
@@ -5,5 +8,6 @@ def test_create_resources(resources, app):
 
     for resource_name, resource_list in resources.items():
         for resource in resource_list:
+            logging.info(f"POSTING {resource_name} {resource}")
             r = app.post(f"/{resource_name}", json=resource)
             assert r.status_code == 201
