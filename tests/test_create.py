59f757336cec5da96eb8262226c428c4056766f1
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-12-15 15:17:03 +0000

    feat: add fine-grained access tests

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,13 +1,39 @@
 import logging
 
+from conftest import user_headers
 
-def test_create_resources(resources, app):
+
+def test_create_resources(resources, app, superuser_headers, admin_user_id):
     """
     This test runs through all resources (in order) and makes sure they can be posted to the app
     """
 
-    for resource_name, resource_list in resources.items():
-        for resource in resource_list:
+    USERS = {resource["id"]: resource for resource in resources["owners"]}
+
+    # post static and types
+    for resource_name in ["certifications", "species"]:
+        for resource in resources[resource_name]:
             logging.info(f"POSTING {resource_name} {resource}")
-            r = app.post(f"/{resource_name}", json=resource)
+            r = app.post(
+                f"/{resource_name}",
+                json=resource,
+                headers=user_headers(USERS[admin_user_id]),
+            )
+            assert r.status_code == 201
+
+    # post users
+    for resource in USERS.values():
+        logging.info(f"POSTING owners {resource}")
+        r = app.post("/owners", json=resource, headers=superuser_headers)
+        assert r.status_code == 201
+
+    # post data
+    for resource_name in ["pets"]:
+        for resource in resources[resource_name]:
+            logging.info(f"POSTING {resource_name} {resource}")
+            r = app.post(
+                f"/{resource_name}",
+                json=resource,
+                headers=user_headers(USERS[resource["owner_id"]]),
+            )
             assert r.status_code == 201
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,4 +1,4 @@
-import logging
+from conftest import user_headers
 
 
 def test_read_resources(setup_and_fill_db, models, resources, app):
@@ -6,8 +6,50 @@ def test_read_resources(setup_and_fill_db, models, resources, app):
     This test uses the ids of each resource to read from the database.
     """
 
-    for resource_name, resource_list in resources.items():
-        for resource in resource_list:
-            r = app.get("/{}/{}".format(resource_name, resource["id"]))
-            logging.info(r.json())
-            assert r.status_code == 200
+    USERS = {resource["id"]: resource for resource in resources["owners"]}
+
+    # each user can read their own data
+    for user_id, user in USERS.items():
+        r = app.get(f"/owners/{user_id}", headers=user_headers(user))
+        assert r.status_code == 200
+        assert r.json().get("id") == user_id
+
+        # any user can read static data
+        for resource_name in ["certifications", "species"]:
+            for resource in resources[resource_name]:
+                _id = resource.get("id")
+                r = app.get(f"/{resource_name}/{_id}", headers=user_headers(user))
+                assert r.status_code == 200
+                assert r.json().get("id") == _id
+
+        # a user can read their own pets
+        for pet in resources["pets"]:
+            if pet["owner_id"] == user_id:
+                r = app.get(f"/pets/{pet['id']}", headers=user_headers(user))
+                assert r.status_code == 200
+                assert r.json().get("id") == pet["id"]
+
+
+def test_read_failcases(setup_and_fill_db, models, resources, app):
+
+    USERS = {resource["id"]: resource for resource in resources["owners"]}
+    PETS = {resource["id"]: resource for resource in resources["pets"]}
+
+    # users can't read other users
+    first_user, second_user = USERS["bonita_leashley"], USERS["pawdrick_pupper"]
+
+    r = app.get(
+        "/owners/{}".format(first_user["id"]), headers=user_headers(second_user)
+    )
+    assert r.status_code == 404
+
+    # users can't read other users' pets
+
+    private_pet = PETS["waffles"]
+    public_pet = PETS["bacon"]
+
+    r = app.get("/pets/{}".format(private_pet["id"]), headers=user_headers(first_user))
+    assert r.status_code == 404
+
+    # ... unless they're public
+    r = app.get("/pets/{}".format(public_pet["id"]), headers=user_headers(first_user))
